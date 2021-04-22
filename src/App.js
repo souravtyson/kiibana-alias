9e20ef20473331fad33c64e5478eee8055f0146b
souravtyson <souravtyson@gmail.com>
2021-04-23 00:52:43 +0530

    added chart and drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import "./styles.css";
+import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+
+const state = {
+  labels: ["January", "February", "March", "April", "May"],
+  datasets: [
+    {
+      label: "Rainfall",
+      backgroundColor: "rgba(75,192,192,1)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+};
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="flex-class">
+      <div className="card card-content">
+        <Line
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: "right"
+            }
+          }}
+        />
+      </div>
+      <div className="card card-content">
+        <Bar
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: "right"
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+import { Drawer, IconButton } from "@material-ui/core";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import List from "@material-ui/core/List";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Drawer variant="persistent" anchor="left">
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   </StrictMode>,
   rootElement
 );
