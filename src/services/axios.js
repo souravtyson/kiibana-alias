faf4664518e002df32adaaabbb1bd095e8e43917
vinayakinsightzz <vinayak.nri@gmail.com>
2021-06-20 11:21:46 +0530

    axios, package update

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: "http://localhost:3001/"               // development
+    //baseURL: "http://localhost:3001/"               // development
     baseURL: "http://localhost:5000/api/sonicwall/"                                 // production
 })
 
