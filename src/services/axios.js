dfb249df81dfc6690a32d532dcc67398bb533b30
vinayakinsightzz <vinayak.nri@gmail.com>
2021-06-20 11:41:32 +0530

    all UI related changes have been pushed

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: "http://localhost:3001/"               // development
-    baseURL: "http://localhost:5000/api/sonicwall/"                                 // production
+     baseURL: "http://localhost:5000/api/sonicwall/"                                 // production
 })
 
 export default instance;
